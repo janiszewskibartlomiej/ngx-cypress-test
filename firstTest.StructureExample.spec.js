15f5d6c0975ecc311e04d5beefdfc18008c33016
Bartlomiej Janiszewski <43078086+janiszewskibartlomiej@users.noreply.github.com>
2021-09-15 12:23:27 +0200

    Create firstTest.StructureExample.spec.js

---
+++ firstTest.StructureExample.spec.js
@@ -0,0 +1,47 @@
+
+describe('Our first suite', () => {
+
+    describe('Our sute section', () => {
+
+        beforeEach('code fore every test', () => {
+            // code  
+          })
+      
+          it('some test name', () => {
+            
+          })
+      
+          it('some test name', () => {
+              
+          })
+      
+          it('some test name', () => {
+              
+          })
+      
+    })
+
+    
+    it('first test', () => {
+
+    })
+
+    it('second test', () => {
+        
+    })
+
+    it('third test', () => {
+        
+    })
+})
+
+describe('Our second suite', () => {
+    
+    it('first test', () => {
+
+    })
+
+    it('second test', () => {
+        
+    })
+})
